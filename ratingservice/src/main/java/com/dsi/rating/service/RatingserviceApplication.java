f2ea334050e8b2f4992c167ad2f17d221f19d88c
shamim7999 <10shamimsarker01@gmail.com>
2024-04-29 11:04:05 +0600

    feat: added service registry

--- ratingservice/src/main/java/com/dsi/rating/service/RatingserviceApplication.java
+++ ratingservice/src/main/java/com/dsi/rating/service/RatingserviceApplication.java
@@ -2,8 +2,10 @@ package com.dsi.rating.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RatingserviceApplication {
 
 	public static void main(String[] args) {
---
+++ registryservice/src/test/java/com/dsi/registry/service/RegistryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsi.registry.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
